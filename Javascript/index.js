b64fbbb12015bec48fa1593d57472455917c3cf0
rijan0 <95970726+rijan0@users.noreply.github.com>
2025-09-08 03:52:04 +0900

    css change
    ui changed

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,8 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const menuIcon = document.getElementById("menu-icon");
-    const navbar = document.getElementById("navbar");
+const menuIcon = document.getElementById("menu-icon");
+const navbar = document.getElementById("navbar");
 
-    menuIcon.addEventListener("click", () => {
-        navbar.classList.toggle("active");
-    });
+menuIcon.addEventListener("click", () => {
+    navbar.classList.toggle("active");
 });
